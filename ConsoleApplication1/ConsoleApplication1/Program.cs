47e4932ba3ca3ebf3a07b7cfdaa98b6e3b6c54a9
nyshthefantastic <astroanju80@yahoo.com>
2018-06-08 14:43:57 +0530

    first commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    static class Program
+    {
+        static void Main()
+        {
+           // Console.Write("Enter image file path: ");
+            string imageFilePath = "C:\\Users\\Public\\Pictures\\Sample Pictures\\Desert.jpg";
+            try
+            {
+                MakePredictionRequest(imageFilePath).Wait();
+            }
+            catch (AggregateException a)
+            {
+                Console.WriteLine("Exception"+a);
+
+            }
+           
+
+            Console.WriteLine("\n\n\nHit ENTER to exit...");
+            Console.ReadLine();
+        }
+
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length);
+        }
+
+        static async Task MakePredictionRequest(string imageFilePath)
+        {
+            var client = new HttpClient();
+
+            // Request headers - replace this example key with your valid subscription key.
+            client.DefaultRequestHeaders.Add("Prediction-Key", "c1ef8a3c73da49cb90aa931fa71b77be");
+
+            // Prediction URL - replace this example URL with your valid prediction URL.
+            string url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/b3817ecc-a072-4d6e-8369-d06fe12c6518/image";
+
+            HttpResponseMessage response;
+
+            // Request body. Try this sample with a locally stored image.
+            byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response = await client.PostAsync(url, content);
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+        }
+    }
+}
